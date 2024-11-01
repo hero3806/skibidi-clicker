2c30f7b9f843c7c705862f73132be064060e07fe
skibidi gyatt <sussymanxdxd@gmail.com>
2024-11-01 18:07:42 +0530

    import optimzation

--- src/brainrotMaxing/SkibidiApp.java
+++ src/brainrotMaxing/SkibidiApp.java
@@ -2,9 +2,12 @@ package brainrotMaxing;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.*;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class SkibidiApp {
 
